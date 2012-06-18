4949c10774116650ea1266282912857a702c60a2
Daniel Stonier <d.stonier@gmail.com>
2012-06-18 14:07:31 +0900

    concert_master : roslib->rosgraph porting for fuerte.

--- concert_master/src/master.py
+++ concert_master/src/master.py
@@ -13,9 +13,45 @@ import xmlrpclib
 
 # Ros imports
 import roslib; roslib.load_manifest('concert_master')
+import rosgraph
 import rospy
 import zeroconf_comms
 from zeroconf_comms.srv import AddService
+
+##############################################################################
+# Functions
+##############################################################################
+
+try:
+    import urllib.parse as urlparse
+except ImportError:
+    import urlparse
+
+def parse_http_host_and_port(url):     
+    """     
+    Convenience routine to handle parsing and validation of HTTP URL     
+    port due to the fact that Python only provides easy accessors in     
+    Python 2.5 and later. Validation checks that the protocol and host     
+    are set.     
+     
+    @param url: URL to parse     
+    @type  url: str     
+    @return: hostname and port number in URL or 80 (default).     
+    @rtype: (str, int)     
+    @raise ValueError: if the url does not validate     
+    """     
+    # can't use p.port because that's only available in Python 2.5     
+    if not url:     
+        raise ValueError('not a valid URL')     
+    p = urlparse.urlparse(url)     
+    if not p[0] or not p[1]: #protocol and host     
+        raise ValueError('not a valid URL')     
+    if ':' in p[1]:     
+        hostname, port = p[1].split(':')     
+        port = int(port)     
+    else:     
+        hostname, port = p[1], 80     
+    return hostname, port
 
 ##############################################################################
 # Main
@@ -36,8 +72,8 @@ def main():
     request.service.name = rospy.get_param("name","Concert Master")
     request.service.type = "_concert-master._tcp"
     request.service.domain = rospy.get_param("domain","local")
-    request.service.port = roslib.network.parse_http_host_and_port(roslib.rosenv.get_master_uri())[1]
-    
+    request.service.port = rosgraph.network.parse_http_host_and_port(rosgraph.rosenv.get_master_uri())[1]
+
     # Make sure the zeroconf is up and running first
     try:
         rospy.wait_for_service('add_service', 30.0)
@@ -50,9 +86,9 @@ def main():
     except rospy.ROSException:
         rospy.logerr("Concert Master: timed out waiting for the zeroconf node to become available.")
         return
-    
+
     # Find useful things to add here later.
     rospy.spin()
-        
+
 if __name__ == '__main__':
     main()
