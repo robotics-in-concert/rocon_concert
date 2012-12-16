601068b4e58f9638fef2cac889c051cba12769a6
Daniel Stonier <d.stonier@gmail.com>
2012-12-16 23:51:59 +0900

    bugfix start script.

--- rocon_orchestra/scripts/orchestra.py
+++ rocon_orchestra/scripts/orchestra.py
@@ -2,11 +2,10 @@
 
 import roslib
 roslib.load_manifest('rocon_orchestra')
+import rospy
 import rocon_orchestra
-
 
 if __name__ == '__main__':
     rospy.init_node('orchestration')  # , log_level=rospy.DEBUG)
     orchestration = rocon_orchestra.Orchestration()
     rospy.spin()
-    return 0
