df01b178b41fe9327f11fb8669b5097ad2cae3fe
Daniel Stonier <d.stonier@gmail.com>
2013-01-23 18:37:01 +0900

    catkinized.

---
+++ concert_conductor/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['concert_conductor'],
+    package_dir={'': 'src'},
+#    scripts=['scripts/gateway_info',
+#             'scripts/remote_gateway_info'
+#             ],
+    requires=['std_msgs', 'rospy', 'appmanager_msgs', 'concert_msgs', 'gateway_msgs']
+)
+
+setup(**d)
