a4db13d4087fd5f8a7397a1833b8512fe323e81e
Jorge Santos <jsantossimon@gmail.com>
2013-11-02 14:40:40 +0900

    Use rocon_std_msgs messages and services instead of rocon_app_manager_msgs ones. Cleanup legacy service list_apps

--- concert_roles/src/concert_roles/manager.py
+++ concert_roles/src/concert_roles/manager.py
@@ -13,8 +13,7 @@ import std_msgs.msg as std_msgs
 import concert_msgs.msg as concert_msgs
 import concert_msgs.srv as concert_srvs
 import rocon_std_msgs.msg as rocon_std_msgs
-import rocon_app_manager_msgs.msg as rocon_app_mng_msgs   # TODO I think both msg and srv should be in rocon_std_msgs, 
-import rocon_app_manager_msgs.srv as rocon_app_mng_srvs   # TODO I think both msg and srv should be in rocon_std_msgs, 
+import rocon_std_msgs.srv as rocon_std_srvs
 import rocon_utilities
 from .remocon_app import RemoconApp
 #import xmlrpclib
@@ -45,18 +44,18 @@ class RoleManager(object):
     ##########################################################################
 
     def __init__(self):
-        self.concert_name = rospy.get_param('~concert_name')
+        self.concert_name = rospy.get_param('~concert_name', 'concert1')
         self.roles = {}
         self.publishers = self._setup_publishers()
         self.services = self._setup_services()
         self.parameters = self._setup_parameters()
         self.icon = rocon_utilities.icon_resource_to_msg('concert_roles/' + self.concert_name + '.png')
-        self.platform_info = rocon_app_mng_msgs.PlatformInfo(
-#                                os=rocon_std_msgs.PlatformInfo.OS_ANDROID,
-#                                version=rocon_std_msgs.PlatformInfo.VERSION_ANY,
-                                platform=rocon_std_msgs.PlatformInfo.PLATFORM_SMART_DEVICE,
-                                system=rocon_std_msgs.PlatformInfo.SYSTEM_ROSJAVA,
-                                name=self.concert_name, icon=self.icon, robot='___robot___')
+        self.platform_info = rocon_std_msgs.PlatformInfo(
+                                os=rocon_std_msgs.PlatformInfo.OS_LINUX,
+                                version=rocon_std_msgs.PlatformInfo.VERSION_ANY,
+                                platform=rocon_std_msgs.PlatformInfo.PLATFORM_PC,
+                                system=rocon_std_msgs.PlatformInfo.SYSTEM_ROS,
+                                name=self.concert_name, icon=self.icon)
         self._stub_init()
         # Aliases
         self.spin = rospy.spin
@@ -64,11 +63,9 @@ class RoleManager(object):
     def _setup_publishers(self):
         publishers = {}
         publishers['icon']  = rospy.Publisher('~icon', rocon_std_msgs.Icon, latch=True)
-        publishers['info']  = rospy.Publisher('~info', rocon_app_mng_msgs.PlatformInfo, latch=True)
-        # TMP FAKE publishers['roles'] = rospy.Publisher('~roles', concert_msgs.Roles, latch=True)
-        publishers['roles'] = rospy.Publisher('~roles', std_msgs.String, latch=True)
-
-        publishers['apps']  = rospy.Publisher('~app_list', rocon_app_mng_msgs.AppList, latch=True)
+        publishers['info']  = rospy.Publisher('~info', rocon_std_msgs.PlatformInfo, latch=True)
+        publishers['roles'] = rospy.Publisher('~roles', concert_msgs.Roles, latch=True)
+        publishers['apps']  = rospy.Publisher('~app_list', concert_msgs.RoleAppList, latch=True)
 
         return publishers
 
@@ -77,14 +74,9 @@ class RoleManager(object):
         services['get_roles_and_apps'] = rospy.Service('~get_roles_and_apps',
                                                        concert_srvs.GetRolesAndApps,
                                                        self._ros_service_filter_roles_and_apps)
-        services['get_platform_info'] = rospy.Service('~platform_info',
-                                                      rocon_app_mng_srvs.GetPlatformInfo,
+        services['get_platform_info'] = rospy.Service('~get_platform_info',
+                                                      rocon_std_srvs.GetPlatformInfo,
                                                       self._ros_service_get_platform_info)
-
-        services['get_apps_list'] = rospy.Service('~list_apps',
-                                                   rocon_app_mng_srvs.GetAppList,
-                                                    self._process_get_app_list)
-
         return services
 
     def _setup_parameters(self):
@@ -102,8 +94,8 @@ class RoleManager(object):
         hard_coded_roles.list = ['Admin', 'Dev', 'Guzzler']
         self.publishers['icon'].publish(self.icon)
         self.publishers['info'].publish(self.platform_info)
-        self.publishers['roles'].publish('[Admin, Dev, Guzzler]') # FAKE  hard_coded_roles)
-        self.publishers['apps'].publish(rocon_app_mng_msgs.AppList())
+        self.publishers['roles'].publish(hard_coded_roles)
+        self.publishers['apps'].publish(concert_msgs.RoleAppList())
 
         for role in hard_coded_roles.list:
             self.roles[role] = []
@@ -179,22 +171,13 @@ class RoleManager(object):
                 if remocon_app.is_runnable(request.platform_info):
                     role_app_list.remocon_apps.append(remocon_app.to_msg())
             roles_and_apps.data.append(role_app_list)
+        print 'roles_and_apps service result: ',roles_and_apps
         return roles_and_apps
 
     def _ros_service_get_platform_info(self, request):
         '''
           Handle incoming requests to provide platform info, same way robots do.
         '''
-        platform_info = rocon_app_mng_srvs.GetPlatformInfoResponse()
+        platform_info = rocon_std_srvs.GetPlatformInfoResponse()
         platform_info.platform_info = self.platform_info
         return platform_info
-
-    def _process_get_app_list(self, request):
-        '''
-          Fake implementation to make bloody PlatformInfoServiceClient happy
-          Stupid me...  he wants app_list topic, not server.....
-        '''
-        response = rocon_app_mng_srvs.GetAppListResponse()
-        response.available_apps = [] #.extend(self._get_app_list())
-        response.running_apps = []
-        return response
