b3cd1da98c1e0b663061cbff28eca01ca613636b
Daniel Stonier <d.stonier@gmail.com>
2013-11-06 23:28:26 +0900

    refactoring, we use '_''s by convention in rocon, also pep8.

--- concert_service_manager/scripts/add_service_from_file.py
+++ concert_service_manager/scripts/add_service_from_file.py
@@ -5,18 +5,20 @@ import yaml
 from concert_msgs.srv import *
 from concert_msgs.msg import *
 
+
 def load_service_from_file(filename):
     rs = ConcertService()
 
     with open(filename) as f:
         yaml_data = yaml.load(f)
-        rs.name         = yaml_data['name']
-        rs.description  = yaml_data['description']
-        rs.author       = yaml_data['author']
-        rs.linkgraph = linkgraph_to_msg(yaml_data['linkgraph']) 
-        rs.priority   = yaml_data['priority']
+        rs.name = yaml_data['name']
+        rs.description = yaml_data['description']
+        rs.author = yaml_data['author']
+        rs.linkgraph = linkgraph_to_msg(yaml_data['linkgraph'])
+        rs.priority = yaml_data['priority']
 
     return rs
+
 
 def linkgraph_to_msg(impl):
 
@@ -26,14 +28,14 @@ def linkgraph_to_msg(impl):
         node['min'] = node['min'] if 'min' in node else 1
         node['max'] = node['max'] if 'max' in node else 1
 
-        lg.nodes.append(LinkNode(node['id'],node['tuple'],node['min'],node['max'],False))
+        lg.nodes.append(LinkNode(node['id'], node['tuple'], node['min'], node['max'], False))
     for topic in impl['topics']:
         lg.topics.append(LinkConnection(topic['id'], topic['type']))
     for action in impl['actions']:
-        lg.actions.append(LinkConnection(action['id'],action['type']))
+        lg.actions.append(LinkConnection(action['id'], action['type']))
     for edge in impl['edges']:
         lg.edges.append(LinkEdge(edge['start'], edge['finish'], edge['remap_from'], edge['remap_to']))
-    
+
     return lg
 
 rospy.init_node('add_srv')
@@ -43,7 +45,7 @@ filename = rospy.get_param('~filename')
 rs = load_service_from_file(filename)
 
 rospy.wait_for_service('service/add')
-srv = rospy.ServiceProxy('service/add',AddConcertService)
+srv = rospy.ServiceProxy('service/add', AddConcertService)
 
 r = srv(rs)
 #rospy.loginfo(str(r))
--- concert_service_manager/setup.py
+++ concert_service_manager/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['concert_servicemanager'],
+    packages=['concert_service_manager'],
     package_dir={'':'src'}
 )
 
