a3d6fae4811361b0ac665676e89f0ba07dafcb37
Daniel Stonier <d.stonier@gmail.com>
2014-01-20 21:31:27 +0900

    concert_service_roslaunch -> concert_service_link_graph.

--- concert_service_link_graph/setup.py
+++ concert_service_link_graph/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['concert_service_roslaunch'],
+    packages=['concert_service_link_graph'],
     package_dir={'':'src'},
     requires=['rospy', 
               'unique_id',
