dde1ebc317fbb44f2892b22d0739ade46f279868
Jihoon Lee <jihoonlee.in@gmail.com>
2014-02-10 16:42:05 +0900

    concert_service_info first draft

--- concert_service_utilities/setup.py
+++ concert_service_utilities/setup.py
@@ -6,6 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['concert_service_utilities'],
     package_dir={'':'src'},
+    scripts=['scripts/concert_service_info'],
     requires=['rospy',
               'rosunit', 
              ]
