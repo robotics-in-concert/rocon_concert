bf9eef8faf896b976c69f3309de631f70eff85d4
Daniel Stonier <d.stonier@gmail.com>
2014-04-22 19:34:47 +0900

    bifurcating shell/gui processing for concert_conductor_graph

--- concert_conductor/setup.py
+++ concert_conductor/setup.py
@@ -6,9 +6,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['concert_conductor'],
     package_dir={'': 'src'},
-#    scripts=['scripts/gateway_info',
-#             'scripts/remote_gateway_info'
-#             ],
+    scripts=['scripts/concert_conductor_graph',
+             ],
     requires=['std_msgs', 'rospy', 'rosgraph', 'rocon_app_manager_msgs', 'concert_msgs', 'gateway_msgs', 'rocon_utilities']
 )
 
