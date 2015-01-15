9f3b170820abe0d4e5336119c934151195b9ad3b
dwlee <dwlee@yujinrobot.com>
2015-01-15 16:10:26 +0900

    function name change

--- concert_service_manager/src/concert_service_manager/utils.py
+++ concert_service_manager/src/concert_service_manager/utils.py
@@ -23,12 +23,9 @@ def setup_home_dirs(concert_name):
         os.makedirs(get_home(concert_name))
 
 
-def setup_service_home_dirs(concert_name, service_name):
-    if not os.path.isdir(get_service_icon_cache_home(concert_name, service_name)):
-        os.makedirs(get_service_icon_cache_home(concert_name, service_name))
-    if not os.path.isdir(get_service_config_cache_home(concert_name, service_name)):
-        os.makedirs(get_service_config_cache_home(concert_name, service_name))
-
+def setup_service_profile_home_dirs(concert_name, service_name):
+    if not os.path.isdir(get_service_profile_cache_home(concert_name, service_name)):
+        os.makedirs(get_service_profile_cache_home(concert_name, service_name))
 
 def get_home(concert_name):
     '''
@@ -41,21 +38,11 @@ def get_home(concert_name):
     return os.path.join(rospkg.get_ros_home(), 'rocon', concert_name)
 
 
-def get_service_icon_cache_home(concert_name, service_name):
-    '''
-      Retrieve the location of the directory used for storing icons about service.
-
-      @return the rocon remocon icons directory (path object).
-      @type str
-    '''
-    return os.path.join(get_home(concert_name), 'icons')
-
-
-def get_service_config_cache_home(concert_name, service_name):
+def get_service_profile_cache_home(concert_name, service_name):
     '''
       Retrieve the location of the directory used for storing service configuration.
 
       @return the rocon remocon qt settings directory (path object).
       @type str
     '''
-    return os.path.join(get_home(concert_name), 'service_name')
+    return os.path.join(get_home(concert_name), 'services', service_name)
