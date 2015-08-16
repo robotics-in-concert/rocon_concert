a694e3baaf0331637e9a2c14d3c4c5caabbbdbfd
Daniel Stonier <d.stonier@gmail.com>
2015-08-16 23:19:01 +0900

    0.7.0

--- concert_master/doc/conf.py
+++ concert_master/doc/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Solution Configuration'
-copyright = u'2014, Jihoon Lee'
+project = u'Concert Master'
+copyright = u'2014, Daniel Stonier, Jihoon Lee'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -92,7 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'agogo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
