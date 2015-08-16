6b988320d00fcfe5dccc4b81ec2ea79a6d9d77cd
Daniel Stonier <d.stonier@gmail.com>
2015-08-16 23:11:38 +0900

    [rocon_concert] cleaned out unused components for gopher

--- concert_service_link_graph/src/concert_service_link_graph/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-# License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_concert/license/LICENSE
-#
-##############################################################################
-# Imports
-##############################################################################
-
-from .static_link_graph_handler import *
