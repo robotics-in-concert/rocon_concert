fb5cb7b2fd41732439c5f36ed663101b68c9d547
Daniel Stonier <d.stonier@gmail.com>
2015-08-17 07:45:15 +0900

    [concert_master] delete cruft and scaled down launcher.

--- concert_master/src/concert_master/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#
-# License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_concert/license/LICENSE
-#
-##############################################################################
-# Imports
-##############################################################################
